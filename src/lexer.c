b5a872704a9946384fbd5501fbe182b15437ed36
552020 <stefanolombardo@posteo.de>
2023-09-06 16:17:26 +0200

    fix compilation linux

--- src/lexer.c
+++ src/lexer.c
@@ -268,10 +268,10 @@ lexer(t_token *token_arr, char **envp, size_t token_count)
 		{
 			if (lexeme_arr[i].type != L_FILENAME_STDIN
 				|| lexeme_arr[i].type != L_FILENAME_STDOUT)
-				lexeme_arr[i] = word_lexeme(&token_arr[i]); // changed this line as well
+				lexeme_arr[i] = word_lexeme(&token_arr[i]);
 		}
 		else
-			break ; // ask what should be done here
+			continue ;
 		i++;
 	}
 	i = 0;
