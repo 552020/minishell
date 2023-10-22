845d2e78deeafb0230afe91309350e6775fdc2fe
Stefano Lombardo <slombard@4b0c3.42berlin.de>
2023-10-22 19:59:32 +0200

    fix small compilers errors

--- src/lexer/wrappers.c
+++ src/lexer/wrappers.c
@@ -26,7 +26,7 @@ void	redirect_append_wrapper(t_lexeme *lexeme_arr, t_token *token_arr,
 		size_t *i, size_t token_count)
 {
 	lexeme_arr[*i] = redirect_append_lexeme(&token_arr[*i]);
-	if (*i < token_count - 1 && token_arr[*i + 1].type != L_END)
+	if (*i < token_count - 1 && token_arr[*i + 1].type != T_END)
 	{
 		lexeme_arr[*i + 1] = redirect_out_target_lexeme(&token_arr[*i + 1]);
 		(*i)++;
