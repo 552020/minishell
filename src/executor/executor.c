a41bb2d24c2a7d3d7fc183e871eac71fb5b21b27
552020 <stefanolombardo@posteo.de>
2023-10-24 11:52:53 +0200

    move notes into notes.md

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -410,26 +410,3 @@ void	handle_pipes(t_ast_node *node, char *dir_paths, char **envp,
 			execute(node, dir_paths, envp, env_table);
 	}
 }
-
-// Notes:
-// 1) implement redirections that can be used with and without rl_dump_functions
-// ex1:  cat test.txt | >> test.txt (appends)
-// ex2:  cat test.txt | grep "hello" >> test.txt (appends)
-// ex3:  cat test.txt | grep "hello" >> test.txt | cat test.txt (does not append)
-// 2)implement open file and close file for redirections correctly
-// Errors:
-
-// 1)
-// correct:
-// cat infile.txt | grep h
-// This is computer
-// hello world
-
-// wrong:
-// cat infile.txt | grep h
-// stuck at lexing first round
-
-// 2) cat infile.txt >> outfile.txt | cat outfile.txt (this sometimes appends sometimes not in shell)
-//   our version does not append and I dont know if it should
-
-// 3) echo 123 > infile.txt >> infile >> infile.txt
