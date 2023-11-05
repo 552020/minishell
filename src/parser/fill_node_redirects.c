473499239cd7c717d13b986cc02bfba3ea57293c
Batuhan Sengezer <bsengeze@c1b2c1.42berlin.de>
2023-11-05 21:42:26 +0100

    fixed input output file leak

--- src/parser/fill_node_redirects.c
+++ src/parser/fill_node_redirects.c
@@ -8,12 +8,16 @@ void	handle_simple_redirects(t_lexeme *lexemes, int idx, t_ast_node **node)
 		;
 	else if (lexemes[idx].type == L_FILENAME_STDIN)
 	{
+		if ((*node)->input_file != NULL)
+            free((*node)->input_file);
 		(*node)->input_file = ft_strdup(lexemes[idx].str);
 		if ((*node)->input_file == NULL)
 			print_and_exit("Error: malloc node->input_file failed");
 	}
 	else if (lexemes[idx].type == L_FILENAME_STDOUT)
 	{
+		if ((*node)->output_file != NULL)
+            free((*node)->output_file);
 		(*node)->output_file = ft_strdup(lexemes[idx].str);
 		if ((*node)->output_file == NULL)
 			print_and_exit("Error: malloc node->output_file failed");
@@ -28,12 +32,16 @@ void	handle_double_redirects(t_lexeme *lexemes, int idx, t_ast_node **node)
 		;
 	else if (lexemes[idx].type == L_FILENAME_STDIN)
 	{
+		if ((*node)->input_file != NULL)
+            free((*node)->input_file);
 		(*node)->input_file = ft_strdup(lexemes[idx].str);
 		if ((*node)->input_file == NULL)
 			print_and_exit("Error: malloc node->input_file failed");
 	}
 	else if (lexemes[idx].type == L_FILENAME_STDOUT)
 	{
+		if ((*node)->output_file != NULL)
+            free((*node)->output_file);
 		(*node)->output_file = ft_strdup(lexemes[idx].str);
 		if ((*node)->output_file == NULL)
 			print_and_exit("Error: malloc node->output_file failed");
