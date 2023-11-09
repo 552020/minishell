75427e64b090e966caae13a0e272332f244ecdfc
552020 <stefanolombardo@posteo.de>
2023-11-09 18:30:12 +0100

    remove comments - no mem to free here

--- src/utils/init.c
+++ src/utils/init.c
@@ -11,9 +11,7 @@ void	initialize_data(t_data *data)
 	data->env_table = (t_env_table *)malloc(sizeof(t_env_table));
 	if (!data->env_table)
 	{
-		// Handle memory allocation failure
 		printf("Error: Out of memory\n");
-		// free memory
 		exit(EXIT_FAILURE);
 	}
 }
