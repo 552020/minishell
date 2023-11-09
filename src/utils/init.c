648cae9f93a9c2c679a288726ba3a76ab18f5755
552020 <stefanolombardo@posteo.de>
2023-11-09 15:00:49 +0100

    fix seg fault

--- src/utils/init.c
+++ src/utils/init.c
@@ -7,4 +7,13 @@ void	initialize_data(t_data *data)
 	data->token_arr = NULL;
 	data->lexeme_arr = NULL;
 	data->ast_root = NULL;
+
+	data->env_table = (t_env_table *)malloc(sizeof(t_env_table));
+	if (!data->env_table)
+	{
+		// Handle memory allocation failure
+		printf("Error: Out of memory\n");
+		// free memory
+		exit(EXIT_FAILURE);
+	}
 }
