57f104f1365a03cee5fd67eabf5d38b58c876c05
552020 <stefanolombardo@posteo.de>
2023-11-13 13:41:02 +0100

    remove free cause this is already freed in export and unset

--- src/executor/execute_builtins.c
+++ src/executor/execute_builtins.c
@@ -50,7 +50,6 @@ void	builtin_with_args(t_ast_node *node, t_data *data)
 	else if (ft_strncmp(node->cmd, "unset", 5) == 0 && ft_strlen(node->cmd) == 5
 		&& cmd_and_args_arr)
 		unset(cmd_and_args_arr, data);
-	free_cmd_and_args_arr(cmd_and_args_arr);
 }
 
 void	builtin_without_args(t_ast_node *node, t_data *data)
