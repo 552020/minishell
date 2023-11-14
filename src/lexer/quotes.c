f59de084d3dfc25110c9e051209cbbfa5f36062c
552020 <stefanolombardo@posteo.de>
2023-11-14 14:48:25 +0100

    fix invalid read of size 1

--- src/lexer/quotes.c
+++ src/lexer/quotes.c
@@ -101,7 +101,9 @@ t_lexeme	t_double_quotes_var_subs(t_token *token, t_data *data)
 	lexeme.original = ft_strdup(token->str);
 	if (!lexeme.original)
 		free_exit(data, "Error: malloc lexeme.original failed\n");
-	while ((vars.str = ft_strchr(vars.str, '$')))
+	// while ((vars.str = ft_strchr(vars.str, '$')))
+	while (vars.str != NULL && vars.str[0] != '\0'
+		&& (vars.str = ft_strchr(vars.str, '$')))
 	{
 		process_variable(&vars, token, data);
 		vars.str = token->str + (vars.str - token->str) + ft_strlen(vars.value);
