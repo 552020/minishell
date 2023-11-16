57c082c46a697ed3629dd36fe959d98b4ef24a11
552020 <stefanolombardo@posteo.de>
2023-11-16 14:18:00 +0100

    remove tests dir from gitignore

---
+++ tests/wildcard/test_wildcard.c
@@ -0,0 +1,41 @@
+#include "minishell.h"
+#include "wildcard.h"
+#include <assert.h>
+
+void	test_build_pattern(const char *pattern, const char *expected_pattern)
+{
+	t_pattern	result;
+	const char	*asterisk;
+
+	asterisk = strchr(pattern, '*');
+	if (asterisk)
+	{
+		build_pattern(asterisk, pattern, &result);
+		// Now assert the expected values in result
+		// e.g., assert(strcmp(result.prefix, "expected_prefix") == 0);
+		assert(ft_strncmp(result.pattern, expected_pattern,
+				result.pattern_len) == 0);
+	}
+	else
+	{
+		printf("No asterisk found in pattern: %s\n", pattern);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+		test_build_pattern(argv[1], argv[2]);
+	else if (argc == 1)
+	{
+		test_build_pattern("some words before some*pattern and other after",
+			"some*pattern");
+	}
+	else
+	{
+		printf("Usage: %s [pattern]\n", argv[0]);
+		return (1);
+	}
+	printf("All tests passed!\n");
+	return (0);
+}
