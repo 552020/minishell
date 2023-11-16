7cade76667bc2bb08f86b7b5e5589a8c17a42628
552020 <stefanolombardo@posteo.de>
2023-11-16 13:21:37 +0100

    some changed in the unit test plus build patter itself

--- src/tokenizer/wildcard.c
+++ src/tokenizer/wildcard.c
@@ -1,34 +1,5 @@
 #include "minishell.h"
-#include <dirent.h>
-
-typedef struct s_pattern
-{
-	char		*pattern;
-	char		*prefix;
-	char		*suffix;
-	char		**midfixes;
-	size_t		prefix_len;
-	size_t		suffix_len;
-	size_t		midfixes_nbr;
-	size_t		midfix_len;
-	const char	*start;
-	const char	*end;
-	char		*ret;
-	char		*tmp;
-}				t_pattern;
-
-typedef struct s_entry
-{
-	char		*entry;
-	char		*idx;
-}				t_entry;
-
-typedef struct s_entries_arr
-{
-	t_entry		**entries;
-	t_entry		**matching_entries;
-	int			count;
-}				t_entries_arr;
+#include "wildcard.h"
 
 /* build_pattern
   This function will build a "pattern" from a string containing a wildcard. Example: from a string like "cat file*.txt",
@@ -70,6 +41,12 @@ void	build_pattern(const char *asterisk, const char *input_start,
 	pattern.prefix_len = asterisk - pattern.start;
 	if (pattern.prefix_len > 0)
 		pattern.prefix = ft_substr(pattern.start, 0, asterisk - pattern.start);
+	// Build the pattern
+	pattern.end = pattern.start;
+	while (*pattern.end && !ft_isspace(*pattern.end))
+		pattern.end++;
+	pattern.pattern_len = pattern.end - pattern.start;
+	pattern.pattern = ft_substr(pattern.start, 0, pattern.pattern_len);
 	// Build the midfixes
 	asterisk_reader = (char *)asterisk + 1;
 	while (*asterisk_reader && !ft_isspace(*asterisk_reader))
---
+++ src/tokenizer/wildcard.h
@@ -0,0 +1,29 @@
+#include "minishell.h"
+#include <dirent.h>
+
+typedef struct s_pattern {
+  char *pattern;
+  char *prefix;
+  char *suffix;
+  char **midfixes;
+  size_t pattern_len;
+  size_t prefix_len;
+  size_t suffix_len;
+  size_t midfixes_nbr;
+  size_t midfix_len;
+  const char *start;
+  const char *end;
+  char *ret;
+  char *tmp;
+} t_pattern;
+
+typedef struct s_entry {
+  char *entry;
+  char *idx;
+} t_entry;
+
+typedef struct s_entries_arr {
+  t_entry **entries;
+  t_entry **matching_entries;
+  int count;
+} t_entries_arr;
