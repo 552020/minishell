53c6c4d3f0f9046d867284783041935c23a7812b
Batuhan Sengezer <bsengeze@c4c5c1.42berlin.de>
2023-11-25 02:03:17 +0100

    double frees fixed

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -5,8 +5,12 @@ void	ft_exit(int exit_code, t_ast_node *node, char **envp,
 {
 	// To do : implement flags and exit code
 	// update exit status here which is the argument
-	free_ast(node);
-	free_hash_table(table);
-	free_envp(envp);
+	
+	if(node)
+		free_ast(node);
+	if(table)
+		free_hash_table(table);
+	if(envp)
+		free_envp(envp);
 	exit(exit_code);
 }
