24cb9598cc5100ed17cb0e389429abd37f11dde3
Batuhan Sengezer <bsengeze@c4c5c1.42berlin.de>
2023-11-26 05:55:26 +0100

    removed comments

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,29 +1,5 @@
 #include "minishell.h"
 
-// int	echo(t_ast_node *node)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	if (node->args == NULL)
-// 	{
-// 		printf("\n");
-// 		return (EXIT_SUCCESS);
-// 	}
-// 	if (node->args[0] && ft_strncmp(node->args[0], "-n", 2) == 0)
-// 		i++;
-// 	while (node->args[i])
-// 	{
-// 		printf("%s", node->args[i]);
-// 		if (node->args[i + 1])
-// 			printf(" ");
-// 		i++;
-// 	}
-// 	if (node->args[0] && ft_strncmp(node->args[0], "-n", 2) != 0)
-// 		printf("\n");
-// 	return (EXIT_SUCCESS);
-// }
-
 int	echo(t_ast_node *node)
 {
 	int	i;
@@ -57,42 +33,3 @@ int	echo(t_ast_node *node)
 		printf("\n");
 	return (EXIT_SUCCESS);
 }
-
-// int	echo(t_ast_node *node)
-// {
-// 	int i;
-// 	int j;
-
-// 	i = 0;
-// 	j = 0;
-// 	if (node->args == NULL)
-// 	{
-// 		printf("\n");
-// 		return (EXIT_SUCCESS);
-// 	}
-// 	while (node->args[i] && ft_strncmp(node->args[i], "-n", 2) == 0)
-// 	{
-// 		while (node->args[i][j] && node->args[i][j] == 'n')
-// 			j++;
-// 		i++;
-// 		if (node->args[i][j] != 'n')
-// 			break ;
-// 		j = 0;
-// 	}
-// 	while (node->args[i])
-// 	{
-// 		printf("%s", node->args[i]);
-// 		if (node->args[i + 1])
-// 			printf(" ");
-// 		i++;
-// 	}
-// 	j = 0;
-// 	if (node->args[0] && ft_strncmp(node->args[0], "-n", 2) != 0)
-// 	{
-// 		while (node->args[0][j] && node->args[0][j] == 'n')
-// 			j++;
-// 		if (node->args[0][j] == 'n')
-// 			printf("\n");
-// 	}
-// 	return (EXIT_SUCCESS);
-// }
