02201f0546572986161e40af5919598d4e80ade2
Stefano Lombardo <slombard@4b0c3.42berlin.de>
2023-11-26 04:10:47 +0100

    small aesthetical change

--- src/executor/heredoc.c
+++ src/executor/heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsengeze <bsengeze@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: slombard <slombard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 00:50:07 by bsengeze          #+#    #+#             */
-/*   Updated: 2023/11/12 20:59:30 by bsengeze         ###   ########.fr       */
+/*   Updated: 2023/11/26 04:10:29 by slombard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,8 +66,8 @@ int	ft_heredoc(t_ast_node *node, char *delimiter, t_data *data)
 			if (!line)
 			{
 				close(fd[1]);
-				printf("bash: warning: here-document delimited by end-of-file ");
-				printf("(wanted %s')\n", delimiter);
+				printf("minishell: warning: here-document delimited by end-of-file ");
+				printf("(wanted '%s')\n", delimiter);
 				exit(EXIT_FAILURE);
 			}
 			if (ft_strncmp(line, delimiter, ft_strlen(delimiter)) == 0
