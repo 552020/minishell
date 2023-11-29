422455d0565cf557870faf68eafa15965f949b04
552020 <stefanolombardo@posteo.de>
2023-11-29 05:26:44 +0100

    refine main unit test

--- tests/reshuffle_quotes/test_reshuffle_quotes.c
+++ tests/reshuffle_quotes/test_reshuffle_quotes.c
@@ -21,17 +21,39 @@ void	test_reshuffle_single_quotes(const char *input, const char *expected)
 	free(result);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	// Test cases
-	test_reshuffle_single_quotes("echo 'hello'", "echo 'hello'");
-	test_reshuffle_single_quotes("echo 'he'  'llo'", "echo 'he'  'llo'");
-	test_reshuffle_single_quotes("echo hello", "echo hello");
-	test_reshuffle_single_quotes("echo 'hello", "echo 'hello");
-	test_reshuffle_single_quotes("", "");
-	test_reshuffle_single_quotes(NULL, NULL);
-	test_reshuffle_single_quotes("echo 'he llo' world", "echo 'he llo' world");
-	test_reshuffle_single_quotes("'h''e''l''l''o'", "'hello'");
-	test_reshuffle_single_quotes("he''llo", "'hello'");
+	char	*result;
+
+	if (argc == 1)
+	{
+		// Test cases
+		test_reshuffle_single_quotes("echo 'hello'", "echo 'hello'");
+		test_reshuffle_single_quotes("echo 'he'  'llo'", "echo 'he'  'llo'");
+		test_reshuffle_single_quotes("echo hello", "echo hello");
+		test_reshuffle_single_quotes("echo 'hello", "echo 'hello");
+		test_reshuffle_single_quotes("", "");
+		test_reshuffle_single_quotes(NULL, NULL);
+		test_reshuffle_single_quotes("echo 'he llo' world",
+			"echo 'he llo' world");
+		test_reshuffle_single_quotes("'h''e''l''l''o'", "'hello'");
+		test_reshuffle_single_quotes("he''llo", "'hello'");
+	}
+	if (argc == 2)
+	{
+		result = reshuffle_single_quotes(argv[1]);
+		printf("%s\n", result);
+		free(result);
+	}
+	if (argc == 3)
+	{
+		result = reshuffle_single_quotes(argv[1]);
+		if (strcmp(result, argv[2]) == 0)
+			printf("Test passed (%s).\n", argv[1]);
+		else
+			printf("Test failed (%s). Expected: <%s>, Got: <%s>\n", argv[1],
+				argv[2], result);
+		free(result);
+	}
 	return (0);
 }
