b1cf9832bf18d4be991d1269a3f58786341238d6
552020 <stefanolombardo@posteo.de>
2023-11-29 04:35:24 +0100

    add tests for reshuffle quotes function

---
+++ tests/reshuffle_quotes/test_reshuffle_quotes.c
@@ -0,0 +1,36 @@
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+void	test_reshuffle_single_quotes(const char *input, const char *expected)
+{
+	char	*result;
+
+	result = reshuffle_single_quotes(input);
+	if ((result == NULL && expected == NULL) || (result && expected
+			&& strcmp(result, expected) == 0))
+	{
+		printf("Test passed (%s).\n", input);
+	}
+	else
+	{
+		printf("Test failed (%s). Expected: '%s', Got: '%s'\n", input, expected,
+			result);
+	}
+	free(result);
+}
+
+int	main(void)
+{
+	// Test cases
+	test_reshuffle_single_quotes("echo 'hello'", "echo 'hello'");
+	test_reshuffle_single_quotes("echo 'he'  'llo'", "echo 'he'  'llo'");
+	test_reshuffle_single_quotes("echo hello", "echo hello");
+	test_reshuffle_single_quotes("echo 'hello", "echo 'hello'");
+	test_reshuffle_single_quotes("", "");
+	test_reshuffle_single_quotes(NULL, NULL);
+	test_reshuffle_single_quotes("echo 'he llo' world", "echo 'he llo' world");
+	test_reshuffle_single_quotes("'h''e''l''l''o'", "'hello'");
+	test_reshuffle_single_quotes("he''llo", "he''llo");
+	return (0);
+}
