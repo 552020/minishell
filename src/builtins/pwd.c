61c375d50b0458df724a7e1356f30a4648d9a884
Stefano Lombardo <slombard@4b0c3.42berlin.de>
2023-12-04 23:07:24 +0100

    norminette pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slombard <slombard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 23:07:00 by slombard          #+#    #+#             */
+/*   Updated: 2023/12/04 23:07:11 by slombard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 #define MAX_PATH_SIZE 1024
 
 int	print_working_directory(void)
 {
-	char buffer[MAX_PATH_SIZE];
+	char	buffer[MAX_PATH_SIZE];
 
 	if (getcwd(buffer, sizeof(buffer)) != NULL)
 	{
