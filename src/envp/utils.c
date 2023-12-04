5ba9e49e253eb4b53596e25655acdf67563f32bf
Stefano Lombardo <slombard@4b0c3.42berlin.de>
2023-12-04 22:20:23 +0100

    norminette fuck this shit

--- src/envp/utils.c
+++ src/envp/utils.c
@@ -6,7 +6,7 @@
 /*   By: slombard <slombard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 22:06:35 by slombard          #+#    #+#             */
-/*   Updated: 2023/12/04 22:06:51 by slombard         ###   ########.fr       */
+/*   Updated: 2023/12/04 22:19:48 by slombard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,46 +30,64 @@ char	*ft_getenv(t_env_var **table, const char *key)
 	return (NULL);
 }
 
-char	**hash_table_to_arr(t_data *data)
+typedef struct s_hash_table_to_arr
 {
 	int			i;
 	int			j;
 	char		**envp;
 	char		*temp;
 	t_env_var	*node;
+}				t_hash_table_to_arr;
 
+int	init_t_hash_table_to_arr_and_checks(t_hash_table_to_arr *vars, t_data *data)
+{
+	vars->i = 0;
+	vars->j = 0;
+	vars->envp = NULL;
+	vars->temp = NULL;
+	vars->node = NULL;
 	if (data->env_table->count == 0)
-		return (NULL);
+		return (0);
 	if (data->env_arr != NULL)
 		free_envp(data->env_arr);
-	envp = (char **)malloc(sizeof(char *) * (data->env_table->count + 1));
-	if (envp == NULL)
+	vars->envp = (char **)malloc(sizeof(char *) * (data->env_table->count + 1));
+	if (vars->envp == NULL)
 		free_exit(data, "Error: malloc in hash_table_to_arr failed\n");
-	data->env_arr = envp;
-	i = 0;
-	j = 0;
-	while (i < TABLE_SIZE)
+	data->env_arr = vars->envp;
+	return (1);
+}
+
+void	assign_values_while_loop(t_hash_table_to_arr *vars, t_data *data)
+{
+	vars->temp = ft_strjoin(vars->node->key, "=");
+	if (!vars->temp)
+		free_exit(data, "Error: ft_strjoin failed\n");
+	vars->envp[vars->j] = ft_strjoin(vars->temp, vars->node->value);
+	if (!vars->envp[vars->j])
 	{
-		node = data->env_table->table[i];
-		while (node != NULL)
-		{
-			temp = ft_strjoin(node->key, "=");
-			if (!temp)
-				free_exit(data, "Error: ft_strjoin failed\n");
-			envp[j] = ft_strjoin(temp, node->value);
-			if (!envp[j])
-			{
-				free(temp);
-				temp = NULL;
-				free_exit(data, "Error: ft_strjoin failed\n");
-			}
-			free(temp);
-			temp = NULL;
-			j++;
-			node = node->next;
-		}
-		i++;
+		free(vars->temp);
+		vars->temp = NULL;
+		free_exit(data, "Error: ft_strjoin failed\n");
 	}
-	envp[j] = NULL;
-	return (envp);
+	free(vars->temp);
+	vars->temp = NULL;
+	vars->j++;
+	vars->node = vars->node->next;
+}
+
+char	**hash_table_to_arr(t_data *data)
+{
+	t_hash_table_to_arr	vars;
+
+	if (!init_t_hash_table_to_arr_and_checks(&vars, data))
+		return (NULL);
+	while (vars.i < TABLE_SIZE)
+	{
+		vars.node = data->env_table->table[vars.i];
+		while (vars.node != NULL)
+			assign_values_while_loop(&vars, data);
+		vars.i++;
+	}
+	vars.envp[vars.j] = NULL;
+	return (vars.envp);
 }
