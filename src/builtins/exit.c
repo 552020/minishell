1a04a7bdba47076b16f0f56ce9e6ff511c3a1a02
552020 <stefanolombardo@posteo.de>
2023-12-05 16:44:37 +0100

    WIP exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -12,7 +12,26 @@
 
 #include "minishell.h"
 
-int	exit_input_check(char *str)
+int	check_parenthesis(t_ast_node *cmd)
+{
+	if (cmd->cmd[4] == '(' && cmd->cmd[ft_strlen(cmd->cmd) - 1] == ')')
+		return (1);
+	return (0);
+}
+
+int	build_exit_code(char *str)
+{
+	int	exit_code;
+
+	exit_code = ft_atoi(str);
+	while (exit_code > 255)
+		exit_code -= 256;
+	while (exit_code < 0)
+		exit_code += 256;
+	return (exit_code);
+}
+
+int	exit_input_check(char *str, int *exit_code, t_ast_node *node)
 {
 	int	i;
 
@@ -22,74 +41,66 @@ int	exit_input_check(char *str)
 		if (str[i] == '+' || str[i] == '-')
 			i++;
 		if (!ft_isdigit(str[i]))
-			return (0);
+		{
+			ft_putstr_fd(" numeric argument required\n", STDERR_FILENO);
+			return (2);
+		}
 		i++;
 	}
+	if (node->args[1])
+		ft_putstr_fd(" too many arguments\n", STDERR_FILENO);
+	if (node->args[0])
+		*exit_code = build_exit_code(node->args[0]);
+	if (check_parenthesis(node) && !node->args)
+		*exit_code = build_exit_code(str);
 	return (1);
 }
 
-int	ft_exit(t_ast_node *node, char **envp, t_env_table *table)
+void	free_ft_exit(t_ast_node *node, char **envp, t_env_table *table)
 {
-	int		exit_code;
-	int		exit_status;
-	char	*num_str;
-
-	exit_code = 0;
-	if (ft_strlen(node->cmd) == 4 && !node->args)
-		exit_code = 0;
-	else if (node->args[1])
-	{
-		if (!exit_input_check(node->args[0]))
-		{
-			ft_putstr_fd(" numeric argument required\n", STDERR_FILENO);
-			exit_status = 2;
-			return (exit_status);
-		}
-		ft_putstr_fd(" too many arguments\n", STDERR_FILENO);
-		exit_status = 1;
-		return (exit_status);
-	}
-	else if (ft_strlen(node->cmd) == 4 && node->args[0])
-	{
-		if (!exit_input_check(node->args[0]))
-		{
-			ft_putstr_fd(" numeric argument required\n", STDERR_FILENO);
-			exit_status = 2;
-			return (exit_status);
-		}
-		exit_code = ft_atoi(node->args[0]);
-		while (exit_code > 255)
-			exit_code -= 256;
-		while (exit_code < 0)
-			exit_code += 256;
-	}
-	else if (node->cmd[4] == '(' && node->cmd[ft_strlen(node->cmd) - 1] == ')'
-		&& !node->args)
-	{
-		num_str = ft_substr(node->cmd, 5, ft_strlen(node->cmd) - 6);
-		if (!exit_input_check(num_str))
-		{
-			ft_putstr_fd(" numeric argument required\n", STDERR_FILENO);
-			exit_status = 2;
-			return (exit_status);
-		}
-		exit_code = ft_atoi(num_str);
-		while (exit_code > 255)
-			exit_code -= 256;
-		while (exit_code < 0)
-			exit_code += 256;
-	}
-	else
-	{
-		ft_putstr_fd("syntax error\n", STDERR_FILENO);
-		exit_status = 2;
-		return (exit_status);
-	}
 	if (node)
 		free_ast(node);
 	if (table)
 		free_hash_table(table);
 	if (envp)
 		free_envp(envp);
+}
+
+int	ft_exit(t_ast_node *node, char **envp, t_env_table *table)
+{
+	int		exit_code;
+	int		return_status;
+	char	*num_str;
+
+	exit_code = 0;
+	if (node->args[1])
+	{
+		return_status = exit_input_check(node->args[0], &exit_code, node);
+		if (return_status)
+			return (return_status);
+	}
+	else if (ft_strlen(node->cmd) == 4 && node->args[0])
+	{
+		return_status = exit_input_check(node->args[0], &exit_code, node);
+		if (return_status)
+			return (return_status);
+	}
+	else if (check_parenthesis(node) && !node->args)
+	{
+		num_str = ft_substr(node->cmd, 5, ft_strlen(node->cmd) - 6);
+		return_status = exit_input_check(num_str, &exit_code, node);
+		if (return_status)
+		{
+			free(num_str);
+			return (return_status);
+		}
+		free(num_str);
+	}
+	else
+	{
+		ft_putstr_fd("syntax error\n", STDERR_FILENO);
+		return (2);
+	}
+	free_ft_exit(node, envp, table);
 	exit(exit_code);
 }
